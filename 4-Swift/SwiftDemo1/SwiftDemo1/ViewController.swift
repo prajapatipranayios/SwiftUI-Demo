d191c3b65e52b9051a2c7b1c77087b3e8ded4c90
pranay-L53 <pranay.prajapati@auxanoglobalservices.com>
2024-10-31 17:57:10 +0530

    311024-Calendar Selection Done

--- 4-Swift/SwiftDemo1/SwiftDemo1/ViewController.swift
+++ 4-Swift/SwiftDemo1/SwiftDemo1/ViewController.swift
@@ -28,53 +28,17 @@ class ViewController: UIViewController {
     }
     
     @IBAction func btnDisplayInputDialogTap(_ sender: UIButton) {
-        self.setupDatePicker()
+        //self.setupDatePicker()
     }
     
     @IBAction func btnDisplayMessageDialogTap(_ sender: UIButton) {
         
     }
     
-    
-    
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
     
-    // Function to configure and show the custom date picker
-    private func setupDatePicker() {
-        let calendarVC = CalendarViewController()
-        
-        // Customize the calendar settings
-        //calendarVC.selectionMode = .multiple  // Options: .single, .multiple, .range
-        calendarVC.selectionMode = .range  // Options: .single, .multiple, .range
-        calendarVC.selectionColor = .green  // Custom selection color
-        calendarVC.selectionShape = .round    // Options: .round, .square, .custom
-        //calendarVC.monthsBefore = 2
-        calendarVC.monthsAfter   = 2
-        calendarVC.disablePastDates = true
-        
-        // Custom date format example
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "dd-MM-yyyy"
-        calendarVC.customDateFormatter = dateFormatter
-        
-        // Set up the closure to receive selected dates
-        calendarVC.onDatesSelected = { selectedDates in
-            print("Selected Dates: \(selectedDates)")  // Handle dates as needed
-        }
-        
-        // Present the CalendarViewController
-        self.present(calendarVC, animated: true, completion: nil)
-    }
-    
-    // Function to handle selected dates
-    func handleDateSelection(dates: [String]) {
-        print("Selected Dates: \(dates)")
-        // No need to remove the date picker manually; it's done in CustomDatePickerView
-    }
 }
 
